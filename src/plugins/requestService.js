b7983985b795adccaca172248906ad4db0f6e2ad
rangseyhome <info.rangsey@gmail.com>
2025-01-02 10:19:32 +0700

    change api url

--- src/plugins/requestService.js
+++ src/plugins/requestService.js
@@ -10,8 +10,8 @@ import authHeaderCommon from "./authHeaderCommon";
 // const API_URL = "http://localhost:3000/v1/";
 // const API_COMMON_URL = "http://localhost:3000/v1/"; 
 
-const API_URL = "https://api.ezetech.online/v1/";
-const API_COMMON_URL = "https://api.ezetech.online/v1/"; 
+const API_URL = "https://cadt-proj-1-api.rphvibes.com/v1/";
+const API_COMMON_URL = "https://cadt-proj-1-api.rphvibes.com/v1/"; 
 //common url
 const common_get = (endpoint) => {
     return axios.get(API_COMMON_URL + endpoint, { headers: authHeader() });
